7557873ad2c280f93aeaa506408d2f19edd3c54b
Suyash <suyashmadhesia@gmail.com>
2022-10-02 23:42:00 +0530

    first bootloader program

---
+++ boot.asm
@@ -0,0 +1,42 @@
+ORG 0
+BITS 16
+_start:
+    jmp short start
+    nop             
+times 33 db 0
+
+start:
+    jmp 0x7c0:step
+step:
+    cli ; clear Interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; set enable Interrupts
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+.loop:
+    lodsb ; it will load from si into al register
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret 
+
+print_char:
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+message: db 'Hello! from Suyash. First boot loader', 0
+
+times 510-($ - $$) db 0
+dw 0xAA55
