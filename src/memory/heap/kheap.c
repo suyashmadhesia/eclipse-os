909229609a1bae3d0b582d3b45fd797264d10d89
Suyash <suyashmadhesia@gmail.com>
2022-10-14 20:59:15 +0530

    implementation of struct of heap

---
+++ src/memory/heap/kheap.c
@@ -0,0 +1,21 @@
+#include "kheap.h"
+#include "heap.h"
+#include "../../config.h"
+#include "kernel.h"
+
+struct heap kernel_heap;
+
+struct heap_table kernel_heap_table;
+
+void kheap_init()
+{
+    int total_table_entries = OS_HEAP_SIZE_BYTES / OS_HEAP_BLOCK_SIZE;
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY *)(OS_HEAP_TABLE_ADDRESS);
+    kernel_heap_table.total = total_table_entries;
+    void *end = (void *)(OS_HEAP_ADDRESS + OS_HEAP_SIZE_BYTES);
+    int res = heap_create(&kernel_heap, (void *)OS_HEAP_ADDRESS, end, &kernel_heap_table);
+    if (res < 0)
+    {
+        print("Failed to create heap\n");
+    }
+}
